557d47cf1bcd3c8251747372c0e2db757db81c46
JoJo Hsu <jojo.hsuu@gmail.com>
2014-01-09 17:46:24 +0800

    init

--- Gemfile
+++ Gemfile
@@ -2,6 +2,6 @@ source :rubygems
 gem "sinatra"
 gem 'tilt', '~> 1.4.1'
 gem "datamapper"
-gem "slim"
-gem "dm-postgres-adapter", :group => :production
-gem "dm-sqlite-adapter", :group => :development
+gem 'json'
+gem "haml"
+gem "dm-sqlite-adapter"
--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'data_mapper'
-require 'slim'
+require 'haml'
+require 'json'
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite:development.db")
 set :public_folder, File.dirname(__FILE__) + '/static'
@@ -10,14 +11,6 @@ class Task
   property :id,            Serial
   property :name,          String, required: true
   property :completed_at,  DateTime
-  belongs_to :list
-end
-
-class List
-  include DataMapper::Resource
-  property :id,            Serial
-  property :name,          String, required: true
-  has n, :tasks, :constraint => :destroy
 end
 
 DataMapper.finalize
@@ -25,35 +18,7 @@ DataMapper.finalize
 # Task Control
 
 get '/' do
-  @lists = List.all(:order => [:name])
-  slim :index
-end
-
-post '/:id' do
-  List.get(params[:id]).tasks.create params['task']
-  redirect '/'
-end
-
-delete '/task/:id' do
-  Task.get(params[:id]).destroy
-  redirect '/'
-end
-
-put '/task/:id' do
-  task = Task.get params[:id]
-  task.completed_at = task.completed_at.nil? ? Time.now : nil
-  task.save
-  redirect '/'
-end
-
-# List Control
-
-post '/new/list' do
-  List.create params['list']
-  redirect '/'
-end
-
-delete '/list/:id' do
-  List.get(params[:id]).destroy
-  redirect '/'
+  @tasks = Task.all
+  @tasks = @tasks.to_json
+  haml :index
 end
